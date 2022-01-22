5cbb60dfedb0e998de956e75ccc00e8502b40bb4
Ashlesh Sortee <sorteeashlesh@gmail.com>
2022-01-22 12:46:58 +0530

    Add deploy script for Goerli testnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const {ADMIN_1, ADMIN_2} = process.env;
+  const StakeToken = await ethers.getContractFactory("StakeToken");
+  const stk = await StakeToken.deploy("100000000000000000000000000000");
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await stk.deployed();
 
-  await greeter.deployed();
+  console.log("StakingToken deployed to:", stk.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+  const Staking = await ethers.getContractFactory("Staking");
+  const staking = await upgrades.deployProxy(
+    Staking,
+    [
+      ADMIN_1,
+      ADMIN_2,
+      stk.address
+    ],
+    {
+      initializer: "initialize",
+      kind: "uups",
+    }
+  );
+
+  console.log("Staking deployed to:", staking.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
